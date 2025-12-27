e65c5835a2b13cd6599204f85e38ddebd623efe7
carpediem94 <laquoiboniste94@gmail.com>
2025-12-27 23:59:32 +0900

    ニュース一覧、詳細にISRを採用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,7 +4,7 @@ import { NEWS_LIST_LIMIT } from "../_constants";
 import Pagination from "../_components/Pagination/pagination";
 import SearchField from "../_components/Searchfield/searchfield";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function page() {
   const { contents: news, totalCount } = await getNewsList({
